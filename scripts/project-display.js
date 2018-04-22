f092ee475c9f30aff0b37c16d1cc172b8e314911
Amit Kumar Sarangi <amitkumarsarangi@users.noreply.github.com>
2018-04-22 11:29:06 +0200

    Add back and forward buttons for embeded pages, test only

--- scripts/project-display.js
+++ scripts/project-display.js
@@ -1,12 +1,22 @@
 var projectDisplay = document.getElementById("projectDisplay");
 	var projectDisplayScreen = document.getElementById("projectDisplayScreen");
 	var projectDisplayCloseButton = document.getElementById("projectDisplayCloseButton");
+	var projectDisplayBackButton = document.getElementById("projectDisplayBackButton");
+	var projectDisplayForwardButton = document.getElementById("projectDisplayForwardButton");
 	var projectOpenNewTab = document.getElementById("projectOpenNewTab");
 
 var tabLink = '';
 
 projectDisplayCloseButton.onclick = function() {
 	removeProject();
+}
+
+projectDisplayBackButton.onclick = function() {
+	projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.back();
+}
+
+projectDisplayForwardButton.onclick = function() {
+	projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.forward();
 }
 
 projectOpenNewTab.onclick = function() {
