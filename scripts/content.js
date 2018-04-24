b3153a0ca746833876dda3567a7a9ecf56f783f0
Amit Kumar Sarangi <amitkumarsarangi@users.noreply.github.com>
2018-04-24 14:53:21 +0200

    Add functionality to go full screen

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,7 @@
 var contentContainer = document.getElementsByClassName("content-container")[0];
 
 var aboutMeContent = [
-	'<p>Hello and warm greetings from Amit and thanks a lot for stopping by his lovingly handcrafted site. This website is an expression of his digital side and an open ground for showcasing his acumen in various web technologies.</p>'
+	'<p>Hello and warm greetings from Amit. Thanks a lot for stopping by his lovingly handcrafted site. This website is an expression of his digital side and an open ground for showcasing his acumen in various web technologies.</p>'
 ,	'<p>He is a JavaScript zealot and a JSON enthusiast. He cannot express enough how much he enjoys learning web technologies and languages like python, JavaScript, Sass, React, MySql, MongoDB, Git and others.</p>'
 ,	'<p>Solving coding challenges is his nice way of spending time. And when he is not busy coding, he is playing this <span class="linker" id="http://gameaboutsquares.com/" onclick="openLinks(this.id)">Game about Squares</span>.</p>'
 ,	'<p>Do check out his various <span class="linker" onclick="getContent(skillsContent)">interests and skills</span> that he keeps polishing.</p>'
@@ -53,36 +53,17 @@ var scribblesContent = [
 ,	'<p id="https://amitkumarsarangi.github.io/Build-a-Personal-Portfolio-Webpage/" style="color:#006400;cursor:pointer" onclick="displayProject(this.id)">&#10085; Build a Personal Portfolio Webpage</p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Build-a-Tribute-Page/" style="color:#006400;cursor:pointer" onclick="displayProject(this.id)">&#10085; Build a Tribute Page</p>'
 ,	'<h2>And the rest of all</h2>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Two-Player-Tic-Tac-Toe/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Two Player Tic Tac Toe<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Three-Concentric-Arches/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Three Concentric Arches<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Rotating-Pyramid/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Rotating Pyramid<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-Countdown-Timer/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Countdown Timer<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Rotating-Prism/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Rotating Prism<sup>(CSS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Rotating-Cube/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Rotating Cube<sup>(CSS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-Stopwatch-Timer/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Stopwatch Timer<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Youtube-Play-Pause-Button/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Youtube Play-Pause Button<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Basic-Parallax/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Basic Parallax<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-JS-Analog-Clock/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A JS Analog Clock<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Double-Helix/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Double Helix<sup>(CSS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-JS-Digital-Clock/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A JS Digital Clock<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Night-Vision-Slider/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Night Vision Slider<sup>(CSS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Basic-Multiplication-Game/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Basic Multiplication Game<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-Surfer-Preloader/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Surfer Preloader<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Night-Vision/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Night Vision<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-Bouncing-Dot-Preloader/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Bouncing Dot Preloader<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/A-Preloader-Animation/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Preloader Animation<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Location-Preloader/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Location Preloader<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Simple-Smooth-Scroller/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Simple Smooth Scroller<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Preloader-Animation/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Bubble Preloader<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Simple-To-Do-List/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Simple To-Do List<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Image-Border-For-Image/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Image Border for Image<sup>(CSS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/A-Venice-Postcard/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; A Venice Postcard<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Simple-Image-Slider/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Simple Image Slider<sup>(JS)</sup></p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Three-Rotating-Squares/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Three Rotating Squares<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/Custom-Alert-Dialog-Box/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Custom Alert Dialog Box<sup>(JS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Show-Your-Stack/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Show Your Stack<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Simple-Landing-Page/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Simple Landing Page<sup>(CSS)</sup></p>'
-// ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/Learn-HTML5-Website/main.html" style="cursor:pointer" onclick="displayProject(this.id)">&#10085; Learn HTML5 Website<sup>(CSS)</sup></p>'
 ,	'<hr style="margin:auto;padding:5px 0;width:95%;border-top:1px solid silver">'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-JS/" style="cursor:pointer" onclick="displayProject(this.id)">See All of the JavaScript ones</p>'
 ,	'<p id="https://amitkumarsarangi.github.io/Scrawls-n-Scribbles-CSS/" style="cursor:pointer" onclick="displayProject(this.id)">See All of the CSS ones</p>'
@@ -90,6 +71,7 @@ var scribblesContent = [
 
 var algorithmsContent = [
 	'<h1>Algorithms</h1>'
+,	'<p id="https://medium.com/@sarangiamitkumar/freecodecamp-basic-algorithm-scripting-solutions-reverse-a-string-dafd3f21c172" style="cursor:pointer" onclick="openLinks(this.id)">&raquo; freeCodeCamp Basic Algorithm Scripting Solutions: Reverse a String</p>'
 ];
 
 var postsContent = [
--- scripts/project-display.js
+++ scripts/project-display.js
@@ -13,10 +13,11 @@ var forwardCounter = 0;
 
 projectDisplayCloseButton.onclick = function() {
 	removeProject();
+	doExitFullscreen();
 }
 
 projectDisplayBackButton.onclick = function() {
-	if ((histLen < projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length) && (backCounter < (histLen - projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length))) {
+	if ((histLen < projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length) && (backCounter < (projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length - histLen))) {
 		projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.back();
 		backCounter += 1;
 		forwardCounter -= 1;
@@ -24,7 +25,7 @@ projectDisplayBackButton.onclick = function() {
 }
 
 projectDisplayForwardButton.onclick = function() {
-	if ((histLen < projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length) && (forwardCounter < (histLen - projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length))) {
+	if ((histLen < projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length) && (forwardCounter < (projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length - histLen))) {
 		projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.forward();
 		forwardCounter += 1;
 		backCounter -= 1;
@@ -42,7 +43,32 @@ function displayProject(link) {
 	projectDisplay.style.display = 'block';
 	tabLink = link;
 	histLen = projectDisplayScreen.getElementsByTagName("iframe")[0].contentWindow.history.length;
+	backCounter = 0;
+	forwardCounter = 0;
+	doEnterFullscreen(document.body);
 	dropSound.play();
+}
+
+function doEnterFullscreen(element) {
+	if(element.requestFullscreen)
+		element.requestFullscreen();
+	else if(element.mozRequestFullScreen)
+		element.mozRequestFullScreen();
+	else if(element.webkitRequestFullscreen)
+		element.webkitRequestFullscreen();
+	else if(element.msRequestFullscreen)
+		element.msRequestFullscreen();
+}
+
+function doExitFullscreen() {
+	if(document.exitFullscreen)
+		document.exitFullscreen();
+	else if(document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if(document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if(document.msExitFullscreen)
+		document.msExitFullscreen();
 }
 
 function removeProject() {
