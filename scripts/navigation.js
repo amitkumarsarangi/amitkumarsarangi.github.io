88bfe20250dd4f33f121b1e425aa4a21620c17b4
Amit Kumar Sarangi <amitkumarsarangi@users.noreply.github.com>
2018-04-30 16:48:46 +0200

    Update the navigation icons and navigation toggle

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,17 +1,54 @@
 var navToggler = document.getElementsByClassName("navigation-toggler")[0];
+var canToggle = true;
 
 navToggler.onclick = function() {
-	if (document.getElementsByClassName("header-container")[0].style.height == "100%") {
+	if (document.getElementsByClassName("header-container")[0].style.height == "100%" && canToggle) {
 		closeNavigationPanel();
-	} else {
+		canToggle = false;
+	} else if (canToggle) {
 		openNavigationPanel();
+		canToggle = false;
 	}
-}
-
-function closeNavigationPanel() {
-	document.getElementsByClassName("header-container")[0].style.height = "90px";
 }
 
 function openNavigationPanel() {
 	document.getElementsByClassName("header-container")[0].style.height = "100%";
+	setTimeout(function() {
+		document.getElementsByClassName("navigation-panel")[0].classList.add("navigation-panel-t");
+	} , 1100);
+	setTimeout(function() {
+		for (var i=0; i<document.getElementsByClassName("navigation-section").length; i++) {
+			document.getElementsByClassName("navigation-section")[i].classList.add("navigation-section-t");
+		}
+	} , 1100);
+	setTimeout(function() {
+		for (var i=0; i<document.getElementsByClassName("navigation-label").length; i++) {
+			document.getElementsByClassName("navigation-label")[i].classList.add("navigation-label-t");
+		}
+	} , 1100);
+	setTimeout(function() {
+		for (var i=0; i<document.getElementsByClassName("navigation-icon").length; i++) {
+			document.getElementsByClassName("navigation-icon")[i].classList.add("navigation-icon-t");
+		}
+	} , 1100);
+	setTimeout(function() {
+		canToggle = true;
+	} , 1300);
+}
+
+function closeNavigationPanel() {
+	document.getElementsByClassName("header-container")[0].style.height = "90px";
+	document.getElementsByClassName("navigation-panel")[0].classList.remove("navigation-panel-t");
+	for (var i=0; i<document.getElementsByClassName("navigation-section").length; i++) {
+		document.getElementsByClassName("navigation-section")[i].classList.remove("navigation-section-t");
+	}
+	for (var i=0; i<document.getElementsByClassName("navigation-label").length; i++) {
+		document.getElementsByClassName("navigation-label")[i].classList.remove("navigation-label-t");
+	}
+	for (var i=0; i<document.getElementsByClassName("navigation-icon").length; i++) {
+		document.getElementsByClassName("navigation-icon")[i].classList.remove("navigation-icon-t");
+	}
+	setTimeout(function() {
+		canToggle = true;
+	} , 1300);
 }
