47938f1013f0d85dca110fde6d4dce199fcefbd4
Amit Kumar Sarangi <amitkumarsarangi@users.noreply.github.com>
2018-09-02 07:28:30 +0200

    Complete change in layout and content

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Below this line - openLink and openLinkNewTab functionality -
-var dropSound = new Audio('assets/sounds/dropSound.wav');
-var popSound = new Audio('assets/sounds/popSound.wav');
-
-function openLink(link) {
-	dropSound.play();
-	location.href = link;
-}
-
-function openLinkNewTab(link) {
-	popSound.play();
-	window.open(link, '_blank');
-}
-// Above this line - openLink and openLinkNewTab functionality -
-
 // Below this line - displayProject functionality -
 var lnk = "";
 function displayProject(link) {
@@ -42,36 +27,12 @@ document.getElementsByClassName("project-container-controls")[0].getElementsByTa
 }
 // Above this line - displayProject functionality -
 
-// Below this line - freeCodeCampBasicAlgorithmScriptingSolutions functionality -
-var freeCodeCampBasicAlgorithmScriptingSolutions = document.getElementById("freeCodeCampBasicAlgorithmScriptingSolutions");
-var freeCodeCampBasicAlgorithmScriptingSolutionsArray = document.getElementById("freeCodeCampBasicAlgorithmScriptingSolutionsArray");
-freeCodeCampBasicAlgorithmScriptingSolutions.onclick = function() {
-	if(freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.display === "block") {
-		freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.visibility = "hidden";
-		freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.opacity = 0;
-		setTimeout(function() {
-			freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.display = "none";
-		}, 500);
-	} else {
-		freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.display = "block";
-		setTimeout(function() {
-			freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.visibility = "visible";
-			freeCodeCampBasicAlgorithmScriptingSolutionsArray.style.opacity = 1;
-		}, 500);
-	}
+// Below this line - openLink and openLinkNewTab functionality -
+function openLink(link) {
+	location.href = link;
 }
-// Above this line - freeCodeCampBasicAlgorithmScriptingSolutions functionality -
 
-// Below this line - navigation functionality -
-var navigationToggler = document.getElementsByClassName("navigation-toggler")[0];
-var navigationSection = document.getElementsByClassName("navigation-section")[0];
-navigationToggler.onclick = function() {
-	if(navigationSection.style.bottom === "0%") {
-		navigationSection.style.bottom = "-100%";
-		navigationToggler.style.transform = "rotateZ(0deg)";
-	} else {
-		navigationSection.style.bottom = "0%";
-		navigationToggler.style.transform = "rotateZ(540deg)";
-	}
+function openLinkNewTab(link) {
+	window.open(link, '_blank');
 }
-// Above this line - navigation functionality -
+// Above this line - openLink and openLinkNewTab functionality -
